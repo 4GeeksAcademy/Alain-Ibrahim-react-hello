23ee82e4881ade45b0aa063a3a7a69d6cba1c002
aestradap <55397157+aestradap@users.noreply.github.com>
2024-02-02 03:02:24 +0000

    adding cardsData-List

---
+++ src/js/data/cardsData.jsx
@@ -0,0 +1,64 @@
+export const cardsData = [
+    {
+        'id': 'card-01',
+        'title':'Title',
+        'img_url':'https://hired.com/wp-content/uploads/2022/06/4Geeks-logo.png',
+        'context':'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
+    {
+        
+        'id': 'card-01',
+        'title':'Title',
+        'img_url':'https://hired.com/wp-content/uploads/2022/06/4Geeks-logo.png',
+        'context':'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
+    {
+        
+        'id': 'card-01',
+        'title':'Title',
+        'img_url':'https://hired.com/wp-content/uploads/2022/06/4Geeks-logo.png',
+        'context':'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
+    {
+        
+        'id': 'card-01',
+        'title':'Title',
+        'img_url':'https://hired.com/wp-content/uploads/2022/06/4Geeks-logo.png',
+        'context':'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
+    {
+        
+        'id': 'card-01',
+        'title':'Title',
+        'img_url':'https://hired.com/wp-content/uploads/2022/06/4Geeks-logo.png',
+        'context':'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
+    {
+        
+        'id': 'card-01',
+        'title':'Title',
+        'img_url':'https://hired.com/wp-content/uploads/2022/06/4Geeks-logo.png',
+        'context':'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
+    {
+       
+        'id': 'card-01',
+        'title':'Title',
+        'img_url':'https://hired.com/wp-content/uploads/2022/06/4Geeks-logo.png',
+        'context':'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
+    {
+        
+        'id': 'card-01',
+        'title':'Title',
+        'img_url':'https://hired.com/wp-content/uploads/2022/06/4Geeks-logo.png',
+        'context':'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    },
+    {
+        
+        'id': 'card-01',
+        'title':'Title',
+        'img_url':'https://hired.com/wp-content/uploads/2022/06/4Geeks-logo.png',
+        'context':'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.'
+    }
+];
